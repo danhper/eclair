04a1b30918778b8acb0f2b74f859421e169b1d7c
Daniel Perez <daniel@perez.sh>
2024-06-08 15:33:06 +0100

    Add solang parser

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use rustyline::hint::HistoryHinter;
 use rustyline::validate::MatchingBracketValidator;
 use rustyline::{Completer, Config, Context, Editor, Helper, Hinter, Result, Validator};
 
+use solang_parser::pt::ContractDefinition;
+use solang_parser::{
+    parse,
+    pt::{ContractPart, SourceUnitPart},
+};
+
 #[derive(Helper, Completer, Hinter, Validator)]
 struct MyHelper {
     #[rustyline(Completer)]
@@ -84,10 +90,11 @@ impl rustyline::completion::Completer for MyCompleter {
 }
 
 fn main() -> Result<()> {
-    let config = Config::builder()
-        .completion_type(rustyline::CompletionType::List)
-        .auto_add_history(true)
-        .build();
+    let config =
+        Config::builder()
+            .completion_type(rustyline::CompletionType::List)
+            .auto_add_history(true)
+            .build();
     let history = rustyline::sqlite_history::SQLiteHistory::open(config, "tmp/history.sqlite3")?;
     let h = MyHelper {
         completer: MyCompleter::new(),
@@ -100,21 +107,49 @@ fn main() -> Result<()> {
     let mut rl: Editor<MyHelper, _> = Editor::with_history(config, history)?;
     rl.set_helper(Some(h));
 
-    loop {
-        let p = ">> ";
-        rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{p}\x1b[0m");
-        let readline = rl.readline(p);
-        match readline {
-            Ok(line) => {
-                println!("Line: {}", line);
-            }
-            Err(ReadlineError::Interrupted) => break,
-            Err(ReadlineError::Eof) => break,
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
-        }
+    // loop {
+    //     let p = ">> ";
+    //     rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{p}\x1b[0m");
+    //     let readline = rl.readline(p);
+    //     match readline {
+    //         Ok(line) => {
+    //             println!("Line: {}", line);
+    //         }
+    //         Err(ReadlineError::Interrupted) => break,
+    //         Err(ReadlineError::Eof) => break,
+    //         Err(err) => {
+    //             println!("Error: {:?}", err);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    let mut statement = "BaseContract base = new BaseContract();".to_owned();
+    if !statement.ends_with(';') {
+        statement = format!("{};", statement);
     }
+    let code = format!(r#"contract ReplContract {{
+
+    function replFunction() external {{
+        {}
+    }}
+}}
+"#, statement);
+
+    let (tree, comments) = parse(&code, 0).unwrap();
+    let function_parts = match &tree.0[0] {
+        SourceUnitPart::ContractDefinition(def) => {
+            def.parts.clone()
+        }
+        _ => unreachable!(),
+    };
+    let parts = match &function_parts[0] {
+        ContractPart::FunctionDefinition(def) => {
+            def.body.clone()
+        }
+        _ => unreachable!(),
+    };
+
+    println!("{:#?}", parts);
     Ok(())
 }
