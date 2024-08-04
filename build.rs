b2873f3a6a1a43d17a3430ffbee8e1a674d33498
Daniel Perez <daniel@perez.sh>
2024-08-04 19:53:15 +0100

    Drop vergen dependency

--- build.rs
+++ build.rs
@@ -1,56 +1,37 @@
 use anyhow::Result;
 use git2_rs::Repository;
-use std::collections::BTreeMap;
 
-use vergen::{AddCustomEntries, CargoRerunIfChanged, CargoWarning, DefaultConfig, Emitter};
+fn get_eclair_version() -> Result<(String, Vec<String>)> {
+    let mut warnings = vec![];
 
-#[derive(Default)]
-struct VersionEntryBuilder {}
+    let repo_dir = std::env::current_dir()?;
+    let repo = Repository::discover(repo_dir)?;
+    let ref_head = repo.revparse_single("HEAD")?;
+    let git_sha_buf = ref_head.short_id()?;
+    let git_sha = git_sha_buf.as_str();
 
-impl AddCustomEntries<&str, String> for VersionEntryBuilder {
-    fn add_calculated_entries(
-        &self,
-        _idempotent: bool,
-        cargo_rustc_env_map: &mut BTreeMap<&str, String>,
-        _cargo_rerun_if_changed: &mut CargoRerunIfChanged,
-        cargo_warning: &mut CargoWarning,
-    ) -> Result<()> {
-        let repo_dir = std::env::current_dir()?;
-        let repo = Repository::discover(repo_dir)?;
-        let ref_head = repo.revparse_single("HEAD")?;
-        let git_sha_buf = ref_head.short_id()?;
-        let git_sha = git_sha_buf.as_str();
+    let pkg_version = std::env::var("CARGO_PKG_VERSION")?;
 
-        let pkg_version = std::env::var("CARGO_PKG_VERSION")?;
+    let mut version = format!("Eclair v{}", pkg_version);
 
-        let mut version = format!("Eclair v{}", pkg_version);
-
-        if pkg_version.contains("-dev") {
-            if let Some(git_short_sha) = git_sha {
-                version = format!("{} ({})", version, git_short_sha);
-            } else {
-                cargo_warning.push("Failed to get git short sha".to_string());
-            }
+    if pkg_version.contains("-dev") {
+        if let Some(git_short_sha) = git_sha {
+            version = format!("{} ({})", version, git_short_sha);
+        } else {
+            warnings.push("Failed to get git short sha".to_string());
         }
-
-        cargo_rustc_env_map.insert("ECLAIR_VERSION", version);
-
-        Ok(())
     }
-
-    fn add_default_entries(
-        &self,
-        _config: &DefaultConfig,
-        _cargo_rustc_env_map: &mut BTreeMap<&str, String>,
-        _cargo_rerun_if_changed: &mut CargoRerunIfChanged,
-        _cargo_warning: &mut CargoWarning,
-    ) -> Result<()> {
-        Ok(())
-    }
+    Ok((version, warnings))
 }
 
 fn main() -> Result<()> {
-    Emitter::default()
-        .add_custom_instructions(&VersionEntryBuilder::default())?
-        .emit()
+    let (version, warnings) = get_eclair_version()?;
+
+    for warning in warnings {
+        println!("cargo::warning={}", warning);
+    }
+
+    println!("cargo::rustc-env=ECLAIR_VERSION={}", version);
+
+    Ok(())
 }
